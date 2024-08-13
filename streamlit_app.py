8859989c75ed6383d50c5f4c0ca8012c5ab63cb4
NuthanonTencon <nuthanon.kittiwatanachod@tencon.fi>
2024-08-13 11:27:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,15 @@ import pandas as pd
 import io
 
 uploaded_file = st.file_uploader("Materiaalikohtainen raportti.xlsx", type="xlsx")
+
+def get_elementtityyppi(value):
+    parts = value.split('-')
+    if len(parts) > 1:
+        return parts[0]
+    else:
+        return value[:2]
+
+
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     df = df.drop(df.index[0:3])
@@ -13,7 +22,14 @@ if uploaded_file:
     df = df.fillna(method='ffill')
     df.columns = df.iloc[0]
     df = df[1:]
-    df.insert(0, 'Elementtityyppi', df["Elementtitunnus"].str.split('-', n=1, expand=True)[0])
+
+    df['Elementtityyppi'] = df["Elementtitunnus"].apply(get_elementtityyppi)
+    
+    
+    df = df[df['Elementtityyppi'].str.len() == 2]
+    
+    # Insert the 'Elementtityyppi' column into the DataFrame
+    df.insert(0, 'Elementtityyppi', df.pop('Elementtityyppi'))
 
     towrite = io.BytesIO()
     with pd.ExcelWriter(towrite, engine='xlsxwriter') as writer:
